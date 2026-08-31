81adabf72f76d6d6bd38d30b5c3d7a50d0458f45
Hassan <hassanoft@yahoo.com>
2026-08-31 17:34:48 +0000

    Update basketball.js

--- src/commands/sport/basketball.js
+++ src/commands/sport/basketball.js
@@ -1,1 +1,13 @@
-//bientôt 
+import {
+  checkForceJoin,
+  forceJoinMessage,
+} from '../../utils/forceJoin.js';
+
+export default {
+  name: 'basketball',
+  aliases: [],
+  category: 'sport',
+  description: 'pas de description actuellement...',
+
+  const Message = 'Ya rien à voir actuellement'
+};
